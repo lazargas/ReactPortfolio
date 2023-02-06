67b969985ab206346c40a5bb3abd388c392b1d00
lazargas <82470472+lazargas@users.noreply.github.com>
2023-02-06 12:39:06 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"BIT connections",
+        image:WorkIm,
+        github:"https://github.com/lazargas/ChatBox",
+        live:"",
+    },
+    {
+        id:2,
+        name:"BlockVote",
+        image:RealEst,
+        github:"https://github.com/lazargas/votingDAO",
+        live:"",
+    },
+    {
+        id:3,
+        name:"PortFolio Website",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"Metaverse ventures Private limited",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+
+
+]
